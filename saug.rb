4eef19e5c041b5a9d9c8000cae0aad764aeab63f
Dr. Michael Ulm <michael@mulm.at>
2011-02-02 19:52:35 +0100

    First workable version

--- saug.rb
+++ saug.rb
@@ -26,17 +26,24 @@ require 'time'
 require 'open-uri'
 require 'rss/1.0'
 require 'rss/2.0'
+require 'yaml'
 
 # For test purposes, use fixed data
-Source = "http://www.econlib.org/library/EconTalk.xml"
+ConfigFile = "~/.saugrc/config.yml"
+DownloadsFile = "~/.saugrc/downloads.yml"
+
 TargetDirectory = "~/pod/"
 Verbose = true
+Debug = true
 WeekInSeconds = 7 * 24 * 3600
 
+
 class AggregatorFeed
-  def initialize(source = nil)
+  attr_reader :source, :downloads
+
+  def initialize(source = nil, downloads = nil)
     @source = source
-    @downloaded = []
+    @downloads = (downloads || [])
     @rss = nil
   end
 
@@ -47,7 +54,7 @@ class AggregatorFeed
   def download_conditional(directory, min_date, only_new = true)
     get_rss unless @rss
     @rss.items.each_with_index do |an_item, item_nr|
-      if an_item.date > min_date && ! (only_new && @downloaded.include?(an_item.guid))
+      if an_item.date > min_date && ! (only_new && @downloads.include?(extract_guid(an_item)))
         download(item_nr, directory)
       end
     end
@@ -61,20 +68,71 @@ class AggregatorFeed
     uri = URI.parse(url)
     filename = File.join(File.expand_path(directory), File.basename(uri.path))
 
-    # download url and add guid to @downloaded
-    of = open(filename, 'wb')
-    of.write(open(url).read)
-    of.close
+    # download url and add guid to @downloads
+    unless Debug
+      of = open(filename, 'wb')
+      of.write(open(url).read)
+      of.close
+    end
 
-    @downloaded << @rss.items[item_nr].guid
+    @downloads << extract_guid(@rss.items[item_nr])
 
     puts "Downloaded #{url} to #{filename}" if Verbose
+  end
+
+  private
+
+  # Get guid from feed if existent - else take filename
+  def extract_guid(item)
+    if item.guid && item.guid.content
+      item.guid.content
+    else
+      item.enclosure.url
+    end
+  end
+end
+
+class FeedCollection
+  def initialize(config_file, downloads_file)
+    @config_file = File.expand_path(config_file)
+    @downloads_file = File.expand_path(downloads_file)
+
+    @sources = YAML::load(File.read(@config_file))
+
+    begin
+      @downloads = YAML::load(File.read(@downloads_file))
+    rescue SystemCallError
+      # It is OK when downloads file doesn't exist.
+    end
+    @downloads ||= {}
+
+    @feeds = []
+    @sources.each do |a_source|
+      @feeds << AggregatorFeed.new(a_source, @downloads[a_source])
+    end
+  end
+
+  def download(directory, min_date)
+    @feeds.each do |a_feed|
+      a_feed.download_conditional(directory, min_date)
+    end
+  end
+
+  def save
+    download_data = {}
+    @feeds.each do |a_feed|
+      download_data[a_feed.source] = a_feed.downloads
+    end
+
+    of = File.open(@downloads_file, 'w')
+    of.puts download_data.to_yaml
+    of.close
   end
 end
 
 if $0 == __FILE__
-  feed = AggregatorFeed.new(Source)
-  # download this week's podcast
-  feed.download_conditional(TargetDirectory, Time.now - WeekInSeconds)
+  collection = FeedCollection.new(ConfigFile, DownloadsFile)
+  collection.download(TargetDirectory, Time.now - WeekInSeconds)
+  collection.save
 end
 
